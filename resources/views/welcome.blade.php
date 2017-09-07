dbcb41ddffc5b6798d47f42937b8664674e18c0d
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-09-06 20:41:02 -0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,33 @@
       If you are unsure about how our process works or if you have any questions while you are completing the online form,
       please contact us 24/7 via Email/Chat/Phone # 866.358.9218</p>
 
+      <ul class="features">
+        <li>
+          <i class="fa fa-clock-o" aria-hidden="true"></i>
+          <h3><a href="#">24hr Service</a></h3>
+          <p>Our 24 hour concierge helps reinvent your financial future</p>
+        </li>
+        <li>
+        <li>
+          <i class="fa fa-lock" aria-hidden="true"></i>
+          <h3><a href="#">Security</a></h3>
+          <p>We never run your credit or require personal banking information</p>
+        </li>
+        <li>
+        <li>
+          <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+          <h3><a href="#">Save Money</a></h3>
+          <p>We will always offer the lowest market rates</p>
+        </li>
+        <li>
+        <li>
+          <i class="fa fa-usd" aria-hidden="true"></i>
+          <h3><a href="#">FLux Credit</a></h3>
+          <p>Earn cash back on every settlement!</p>
+        </li>
+        <li>
+      </ul>
+
       <a class="btn" href="/signup">Start Today</a>
     </div>
   </section>
