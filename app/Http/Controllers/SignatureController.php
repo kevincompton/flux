b88f8a3f7293654fe72e3f78e0f3b7041f1b9286
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-09-06 21:00:12 -0700

    update

---
+++ app/Http/Controllers/SignatureController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SignatureController extends Controller
+{
+    
+    public function callback()
+    {
+      return "success";
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/sign', function()
-{
- return "Hello API Event Received";
-});
+Route::post('/sign', 'SignatureController@callback');
 
 Route::get('/', function () {
     return view('welcome');
