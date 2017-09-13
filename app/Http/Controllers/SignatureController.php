af4202870f15ef87239a777dde06e3b13633d83d
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-09-12 17:03:53 -0700

    update

--- app/Http/Controllers/SignatureController.php
+++ app/Http/Controllers/SignatureController.php
@@ -20,6 +20,8 @@ class SignatureController extends Controller
 
       $response = $this->getEmbeddedSignatureRequest();
 
+      return var_dump($response);
+
       $signature_request_id = $response["signature_request"]["signature_request_id"];
       $signatures = $response["signature_request"]["signatures"];
 
