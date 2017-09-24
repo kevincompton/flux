69fe6fe7b1a9337b348a87e77fdb48616ed4637b
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-09-24 14:53:33 -0700

    update

--- app/Mail/NewRegistration.php
+++ app/Mail/NewRegistration.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
 
 class NewRegistration extends Mailable
 {
