9f57f77f73078c1effc16aa56c64cc185a2bda09
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-10-09 19:53:47 -0700

    update

--- resources/views/emails/new_registration.blade.php
+++ resources/views/emails/new_registration.blade.php
@@ -1,10 +1,10 @@
-Dear {{ $request['name'] }},
-
-Welcome to FluxCredit.com! You can login to your Flux dashboard at http://fluxcredit.com/dashboard with:
-
-{{ $request['email'] }}
-{{ $request['password'] }}
-
-Sincerely,
-
+Dear {{ $request['name'] }},<br>
+<br>
+Welcome to FluxCredit.com! You can login to your Flux dashboard at http://fluxcredit.com/dashboard with:<br>
+<br>
+{{ $request['email'] }}<br>
+password: {{ $request['password'] }}<br>
+<br>
+Sincerely,<br>
+<br>
 Your 24 Hour Flux Concierge
