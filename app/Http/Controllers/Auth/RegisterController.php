a4b102f1a0b57cf3e1d0c30a48871f94166db244
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-11-05 00:20:22 -0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -76,7 +76,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => bcrypt($data['password'],
+            'onboard_step' => 0),
         ]);
     }
 }
