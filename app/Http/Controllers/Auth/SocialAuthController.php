ee533d769bd8633ba97121d57ff8199ebd2dba4c
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-11-05 00:18:04 -0700

    update

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -35,7 +35,8 @@ class SocialAuthController extends Controller
             'name'     => $user->name,
             'email'    => $user->email,
             'provider' => $provider,
-            'provider_id' => $user->id
+            'provider_id' => $user->id,
+            'onboard_step' => 0
         ]);
     }
     
