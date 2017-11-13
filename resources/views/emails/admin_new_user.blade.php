90764bc7d3649a7ca515d86a549ac9b2dd41c950
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-11-13 13:05:50 -0800

    update

--- resources/views/emails/admin_new_user.blade.php
+++ resources/views/emails/admin_new_user.blade.php
@@ -3,19 +3,9 @@
   <tr>
     <th>Name</th>
     <th>Email</th>
-    <th>Phone</th>
-    <th>Address</th>
-    <th>City</th>
-    <th>State</th>
-    <th>Zip</th>
   </tr>
   <tr>
     <td>{{ $request['name'] }}</td>
     <td>{{ $request['email'] }}</td>
-    <td>{{ $request['phone'] }}</td>
-    <td>{{ $request['address'] }}</td>
-    <td>{{ $request['city'] }}</td>
-    <td>{{ $request['state'] }}</td>
-    <td>{{ $request['zip'] }}</td>
   </tr>
 </table>
