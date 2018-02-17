2f97446fd5fc59080f14f333930725787022583c
Kevin Compton <kevincompton@Kevins-iMac.local>
2018-02-16 16:59:38 -0800

    update

--- app/Http/Controllers/SignatureController.php
+++ app/Http/Controllers/SignatureController.php
@@ -112,6 +112,8 @@ class SignatureController extends Controller
     private function getEmbeddedCreditSignatureRequest($template_id)
     {
       $user = Auth::user();
+      $cosigner = $user->cosigners()->get()->first();
+      $budget = $user->budget()->get()->first();
       $app = $user->application;
       $ssn = preg_replace ('/^(\d{3})(\d{2})(\d{4})$/', '$1-$2-$3', $user->ssn);
 
@@ -170,7 +172,171 @@ class SignatureController extends Controller
               [
                 'name' => 'user_zip',
                 'value' => $user->zip
-              ]
+              ],
+              [
+                'name' => 'years_at_address',
+                'value' => $app->years_at_address
+              ],
+              [
+                'name' => 'owner_status',
+                'value' => $app->owner_status
+              ],
+              [
+                'name' => 'prev_address',
+                'value' => $app->prev_address
+              ],
+              [
+                'name' => 'prev_city',
+                'value' => $app->prev_city
+              ],
+              [
+                'name' => 'prev_state',
+                'value' => $app->prev_state
+              ],
+              [
+                'name' => 'prev_zip',
+                'value' => $app->prev_zip
+              ],
+              [
+                'name' => 'employer_name',
+                'value' => $app->employer_name
+              ],
+              [
+                'name' => 'employer_phone',
+                'value' => $app->employer_phone
+              ],
+              [
+                'name' => 'position',
+                'value' => $app->position
+              ],
+              [
+                'name' => 'employer_address',
+                'value' => $app->employer_address
+              ],
+              [
+                'name' => 'employer_city',
+                'value' => $app->employer_city
+              ],
+              [
+                'name' => 'employer_state',
+                'value' => $app->employer_state
+              ],
+              [
+                'name' => 'employer_zip',
+                'value' => $app->employer_zip
+              ],
+              [
+                'name' => 'cosigner_name',
+                'value' => $cosigner->name
+              ],
+              [
+                'name' => 'cosigner_name',
+                'value' => $cosigner->name
+              ],
+              [
+                'name' => 'cosigner_dob',
+                'value' => $cosigner->dob
+              ],
+              [
+                'name' => 'cosigner_ssn',
+                'value' => $cosigner->ssn
+              ],
+              [
+                'name' => 'cosigner_dl_no',
+                'value' => $app->cosigner_dl_no
+              ],
+              [
+                'name' => 'cosigner_dl_state',
+                'value' => $app->cosigner_dl_state
+              ],
+              [
+                'name' => 'cosigner_dependencies',
+                'value' => $app->cosigner_dependencies
+              ],
+              [
+                'name' => 'cosigner_address',
+                'value' => $cosigner->address
+              ],
+              [
+                'name' => 'cosigner_city',
+                'value' => $cosigner->city
+              ],
+              [
+                'name' => 'cosigner_state',
+                'value' => $cosigner->state
+              ],
+              [
+                'name' => 'cosigner_zip',
+                'value' => $cosigner->zip
+              ],
+              [
+                'name' => 'cosigner_phone',
+                'value' => $cosigner->phone
+              ],
+              [
+                'name' => 'cosigner_years_at_address',
+                'value' => $app->cosigner_years_at_address
+              ],
+              [
+                'name' => 'cosigner_owner_status',
+                'value' => $app->cosigner_owner_status
+              ],
+              [
+                'name' => 'cosigner_mortgage',
+                'value' => $app->cosigner_mortgage
+              ],
+              [
+                'name' => 'cosigner_prev_address',
+                'value' => $app->cosigner_prev_address
+              ],
+              [
+                'name' => 'cosigner_prev_city',
+                'value' => $app->cosigner_prev_city
+              ],
+              [
+                'name' => 'cosigner_prev_state',
+                'value' => $app->cosigner_prev_state
+              ],
+              [
+                'name' => 'cosigner_prev_zip',
+                'value' => $app->cosigner_prev_zip
+              ],
+              [
+                'name' => 'cosigner_employer_name',
+                'value' => $app->cosigner_employer_name
+              ],
+              [
+                'name' => 'cosigner_employer_phone',
+                'value' => $app->cosigner_employer_phone
+              ],
+              [
+                'name' => 'cosigner_employer_years',
+                'value' => $app->cosigner_prev_zip
+              ],
+              [
+                'name' => 'cosigner_position',
+                'value' => $app->cosigner_position
+              ],
+              [
+                'name' => 'cosigner_income',
+                'value' => $app->cosigner_income
+              ],
+              [
+                'name' => 'cosigner_employer_address',
+                'value' => $app->cosigner_employer_address
+              ],
+              [
+                'name' => 'cosigner_employer_city',
+                'value' => $app->cosigner_employer_city
+              ],
+              [
+                'name' => 'cosigner_employer_state',
+                'value' => $app->cosigner_employer_state
+              ],
+              [
+                'name' => 'cosigner_employer_zip',
+                'value' => $app->cosigner_employer_zip
+              ],
             ])
         ];
 
