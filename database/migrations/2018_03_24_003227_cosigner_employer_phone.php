35462254568dbd1291a3f6cff5c43a4ee3f12ea3
Kevin Compton <kevincompton@Kevins-iMac.local>
2018-03-23 17:34:46 -0700

    update

---
+++ database/migrations/2018_03_24_003227_cosigner_employer_phone.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CosignerEmployerPhone extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->string('cosigner_employer_phone')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
